0bfcf0985fde1fc5bbcb6772fd631dc8a533e22c
Radzhabov Rustam <rrradzhabov@edu.hse.ru>
2024-05-24 20:48:45 +0300

    Add stylish form and replaced logic with post method with cookie.

--- task_5/index.php
+++ task_5/index.php
@@ -3,14 +3,25 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/index.css">
+        <link rel="stylesheet" href="css/form_style.css">
     </head>
-    <form class="UserInfoForm" action="./test.php" method="post">
-        <label for="City">Enter your city</label>
-        <input type="text" name="City" required><br>
-        <label for="Age">Enter your age</label>
-        <input type="number" step="1" name="Age" required><br> 
-        <input type="submit" value="Send" name="UserData">
-    </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            setcookie("city", filter_input(INPUT_POST, 'City'), time() + 30 * 24 * 60 * 60, "/");
+            setcookie("age", filter_input(INPUT_POST, 'Age')  , time() + 30 * 24 * 60 * 60, "/");
+            header("Location: test.php");
+            exit();
+        }
+    ?>
+    <div class="form-container">
+        <h2>User information</h2>
+        <form action="." method="post">
+            <label for="City">Enter your city</label>
+            <input type="text" name="City" required><br>
+            <label for="Age">Enter your age</label>
+            <input type="number" step="1" name="Age" required><br> 
+            <button type="submit">Submit</button>
+        </form>
+    </div>
     </body>
 </html>
--- task_5/test.php
+++ task_5/test.php
@@ -1,26 +1,32 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylesheet" href="css/form_style.css">
+    </head>
     <body>
-        <form>
-        <label for="Age">
-        
-        <?php
-            $city = "";
-            $age  = "";
-            if($_SERVER["REQUEST_METHOD"] == "POST") {
-                $city = filter_input(INPUT_POST, 'City');
-                $age  = filter_input(INPUT_POST, 'Age');
-            }
-            echo "<label for=\"Name\">Your name</label></br>";
-            echo "<input type=\"text\" name=\"Name\"></br>";
-            echo "<label for=\"City\"> Your city:</label></br>";
-            echo "<input type=\"text\" name=\"City\" value=\"$city\"></br>";
-            echo "<label for=\"Age\">Your age:</label></br>";
-            echo "<input type=\"number\" step=\"1\" name=\"Age\" value=\"$age\">"; 
-        ?>
-
-        <?php
-
-        ?>
+        <div class="form-container">
+            <h2>User information</h2>
+            <form>
+                <label for="Age">
+                
+                <?php
+                    $city = "";
+                    $age  = "";
+                    if(isset($_COOKIE["city"])) {
+                        $city = $_COOKIE["city"];
+                    }
+                    if(isset($_COOKIE["age"])) {
+                        $age = $_COOKIE["age"];
+                    }
+                    echo "<label for=\"Name\">Your name</label></br>";
+                    echo "<input type=\"text\" name=\"Name\"></br>";
+                    echo "<label for=\"City\"> Your city:</label></br>";
+                    echo "<input type=\"text\" name=\"City\" value=\"$city\"></br>";
+                    echo "<label for=\"Age\">Your age:</label></br>";
+                    echo "<input type=\"number\" step=\"1\" name=\"Age\" value=\"$age\">"; 
+                ?>
+                <button type="submit">Submit</button>
+            </form>
+        <div>
     </body>
 </html>
